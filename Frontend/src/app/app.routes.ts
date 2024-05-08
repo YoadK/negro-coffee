b176e813288e34c47561d0998a12b6e03e2b2d5a
YoadK <yoadkot@gmail.com>
2024-05-08 19:59:55 +0300

    [ ]-  add Ui for login + register

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -4,6 +4,8 @@ import { HomeComponent } from './components/home-area/home/home.component';
 import { Page404Component } from './components/SharedArea/page404/page404.component';
 import { AddProductComponent } from './components/products-area/add-product/add-product.component';
 import { ProductListComponent } from './components/products-area/product-list/product-list.component';
+import { LoginComponent } from './components/Auth-area/login/login.component';
+import { RegisterComponent } from './components/Auth-area/register/register.component';
 // import { DataListComponent } from './components/data-area/data-list/data-list.component';
 // import { AddDataComponent } from './components/data-area/add-data/add-data.component';
 // import { Page404Component } from './components/layout-area/page404/page404.component';
@@ -12,18 +14,12 @@ import { ProductListComponent } from './components/products-area/product-list/pr
 export const routes: Routes = [
    
 
-    // { path: 'home', component: HomeComponent },
-    // { path: 'data', component: HomeComponent },
-    // { path: 'web-development', component: HomeComponent },
-    // { path: 'design', component: HomeComponent },
-    // { path: 'it-services', component: HomeComponent },
-    // { path: 'contact', component: HomeComponent },
-    // { path: '', redirectTo: '/home', pathMatch: 'full' },
-    // { path: "**", component: Page404Component }
 
     { path: "home", component: HomeComponent },
     { path: "shop", component: ProductListComponent },
-    { path: "new", component: AddProductComponent },
+    { path: "new", component: AddProductComponent },  
+    { path: "login", component: LoginComponent },   
+    { path: "register", component: RegisterComponent },   
     { path: "", redirectTo: "/home", pathMatch: "full" },
     { path: "**", component: Page404Component },
 ];
--- Frontend/src/app/components/Auth-area/auth-menu/auth-menu.component.ts
+++ Frontend/src/app/components/Auth-area/auth-menu/auth-menu.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-auth-menu',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './auth-menu.component.html',
   styleUrl: './auth-menu.component.module.css'
 })
