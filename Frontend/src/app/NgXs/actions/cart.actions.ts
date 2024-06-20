933f679e2e35326897b790227eb944fe768e9afa
YoadK <yoadkot@gmail.com>
2024-06-20 17:09:14 +0300

    [ ] - eod commit.

--- Frontend/src/app/NgXs/actions/cart.actions.ts
+++ Frontend/src/app/NgXs/actions/cart.actions.ts
@@ -2,3 +2,8 @@ export class ToggleCartModal {
     static readonly type = '[Cart] Toggle Cart Modal';
   }
   
+  export class AddToCart {
+    static readonly type = '[Cart] Add To Cart';
+    constructor(public payload: { productId: string, quantity: number }) {}
+  }
+  
